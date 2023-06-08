5ac10d14b8bb0ec9620e4229df5fa8cdf2cc99a4
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2023-06-08 18:10:09 +0530

    mongoDB added

--- server.js
+++ server.js
@@ -1,11 +1,57 @@
 const dotenv=require("dotenv");
+const mongoose=require("mongoose");
 const app=require("./app");
-
-//console.log(app.get("env"));   //development environment
 
 // set the path of .env file
 dotenv.config({path:"./config.env"});
 //console.log(process.env);
+const DB=process.env.DATABASE.replace(
+    "<PASSWORD>",
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB,{
+    useCreateIndex:true,
+    useFindAndModify:false,
+    useNewUrlParser:true,
+}).then((con)=>{
+    //console.log(con.connection);
+    console.log("DB Successfully Connected!");
+});
+
+const nftSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"A NFT must have a name"],
+        unique:true,
+    },
+    rating:{
+        type:Number,
+        default:4.5,
+    },
+    price:{
+        type:Number,
+        required:[true,"A NFT must have price"],
+    }
+});
+
+
+const NFT=mongoose.model("NFT",nftSchema);
+
+const testNFT=new NFT({
+    name:"The smart monkey",
+    rating:3.2,
+    price:576,
+})
+
+testNFT
+    .save()
+    .then((docNFT)=>{
+        console.log(docNFT);
+    })
+    .catch((error)=>{
+        console.log("Error",error);
+    });
 
 const port=process.env.PORT || 8000;
 //to start the server
